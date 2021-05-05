084b6bc4e4fdf02f75acbd2d1abda5a6997b129d
Ar1f007 <muhammadarif9416@yahoo.com>
2021-05-05 17:47:59 +0600

    common styling, navbar, hamburger menu, 3d effect sections

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Common JS
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
 
+document.querySelectorAll(".wrapper").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach((item) => {
+      item.classList.remove("change");
+    });
+  });
+});
+// End of Common JS
