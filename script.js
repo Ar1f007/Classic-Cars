89c6aa86e16b1dd6f1d3a6694c1d2b1e0a4cb1f5
Ar1f007 <muhammadarif9416@yahoo.com>
2021-05-08 23:30:07 +0600

    video gallery

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ document.querySelectorAll(".wrapper").forEach((item) => {
   });
 });
 // End of Common JS
+
+// Section 3: Video Gallery
+const videos = document.querySelectorAll(".video");
+
+videos.forEach((video) => {
+  video.addEventListener("mouseover", () => video.play());
+  video.addEventListener("mouseout", () => video.pause());
+});
+// End of Section 3: Video Gallery
